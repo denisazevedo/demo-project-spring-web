1e2a2f1409f2432c4641a42d7bca4afc54a53bc3
Denis C de Azevedo <denis.azevedo@gmail.com>
2019-04-18 21:13:13 -0300

    fix checkstyle warnings

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,10 +3,20 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application's main class.
+ */
 @SpringBootApplication
-public class Application {
+public final class Application {
 
-    public static void main(String[] args) {
+    /** Private constructor for checkstyle */
+    private Application() {}
+
+    /**
+     * Main method.
+     * @param args
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -5,11 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * Greeting controller.
+ */
 @Controller
 public class GreetingController {
 
+    /**
+     * Greeting method
+     * @param name
+     * @param model
+     * @return
+     */
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public final String greeting(
+            @RequestParam(name = "name", required = false, defaultValue = "World") final String name, 
+            final Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
